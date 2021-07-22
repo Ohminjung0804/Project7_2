840ed5d9a80d3e790b51bef689562c9c09214506
OHMINJUNG <w2009@e-mirim.hs.kr>
2021-07-22 14:14:26 +0900

    edit MainActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2009/project7_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2009/project7_2/MainActivity.java
@@ -1,13 +1,18 @@
 package kr.hs.emirim.w2009.project7_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout linear1;
@@ -21,7 +26,16 @@ public class MainActivity extends AppCompatActivity {
         btnChange = findViewById(R.id.btn_change);
         registerForContextMenu(btnBg);
         registerForContextMenu(btnChange);
+        btnBg.setOnClickListener(btnListener);
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast toast = Toast.makeText(getApplicationContext(),"배경색 변경을 원하면 롱클릭 하세요.", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP|Gravity.LEFT, 100,200);
+            toast.show();
+        }
+    };
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
@@ -33,5 +47,29 @@ public class MainActivity extends AppCompatActivity {
             menu.setHeaderTitle(R.string.btn_change);
             inflater.inflate(R.menu.menu2,menu);
         }
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.item_bg1:
+                linear1.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_bg2:
+                linear1.setBackgroundColor(Color.YELLOW);
+                return true;
+            case R.id.item_bg3:
+                linear1.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.item_rotate:
+                linear1.setRotation(90);
+                return true;
+            case R.id.item_zoom:
+                linear1.setScaleX(2);
+                return true;
+        }
+        return false;
     }
 }
